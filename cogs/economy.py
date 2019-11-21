221f5feb5b40d8c8fcdd02172c9b7ac5eb6d96cb
Audie Ni <audie.ni@yahoo.com>
2019-11-21 13:18:05 -0500

    Changed query to response and fixed minor issues

--- cogs/economy.py
+++ cogs/economy.py
@@ -15,7 +15,9 @@ class Economy(commands.Cog):
             self.db = sqlite3.connect(db)
             self.db_cursor = self.db.cursor()
 
-            self.db_cursor.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, level INTEGER, exp INTEGER)""")
+            self.db_cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, level INTEGER, exp INTEGER)
+            """)
 
         except Error as e:
             print(e)
@@ -25,32 +27,33 @@ class Economy(commands.Cog):
         if message.author.bot or message.content.startswith(config.command_prefix):
             return
 
-        self.db_cursor.execute("SELECT * FROM users WHERE id = ?", [str(message.author.id)])
-        query = self.db_cursor.fetchone()
+        self.db_cursor.execute("SELECT * FROM users WHERE id = ?",[str(message.author.id)])
+        response = self.db_cursor.fetchone()
 
-        if not query:
+        if not response:
             self.db_cursor.execute("INSERT INTO users VALUES(?, ?, ?)", [str(message.author.id), 1, 0])
             self.db.commit()
 
-        user_id, user_level, user_exp = query
+        user_id, user_level, user_exp = response
         user_exp += config.exp_gained
         if int(user_exp / config.exp_leveled) + 1 > user_level:
             await message.channel.send("<@{}> is now level {}.".format(user_id, user_level + 1))
 
         user_level = int(user_exp / config.exp_leveled) + 1
 
-        self.db_cursor.execute("UPDATE users SET level = ?, exp = ? WHERE id = ?", [user_level, user_exp, str(message.author.id)])
+        self.db_cursor.execute("UPDATE users SET level = ?, exp = ? WHERE id = ?",
+            [user_level, user_exp, str(message.author.id)])
         self.db.commit()
 
     @commands.command(aliases=["profile"])
     async def profile_command(self, ctx):
         self.db_cursor.execute("SELECT * FROM users WHERE id = ?", [str(ctx.author.id)])
-        query = self.db_cursor.fetchone()
+        response = self.db_cursor.fetchone()
 
-        if not query:
+        if not response:
             return
 
-        user_id, user_level, user_exp = query
+        user_id, user_level, user_exp = response
 
         await ctx.send("<@{}> is level {} with {} exp.".format(user_id, user_level, user_exp))
 
