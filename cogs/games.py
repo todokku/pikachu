684fd7be66b74b1d6cae3f0473230788fbc05cf5
Audie Ni <audie.ni@yahoo.com>
2019-11-21 15:50:17 -0500

    Fixed a response string

--- cogs/games.py
+++ cogs/games.py
@@ -12,7 +12,7 @@ class Games(commands.Cog):
             number_of_dice = 4
             number_of_sides = 6
             dice = [str(random.choice(range(1, number_of_sides + 1))) for d in range(number_of_dice)]
-            await ctx.send(", ".join(dice) + "You rolled a **{}**!".format(sum(int(d) for d in dice)))
+            await ctx.send(", ".join(dice) + ". You rolled a **{}**!".format(sum(int(d) for d in dice)))
 
 def setup(bot):
     bot.add_cog(Games(bot))
