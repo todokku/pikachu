02f33b75b68192ae9131bf530c011ff866cdf2d0
Audie Ni <13210233+audieni@users.noreply.github.com>
2020-02-22 15:33:02 -0500

    Update main.py

--- pikachu/main.py
+++ pikachu/main.py
@@ -31,15 +31,15 @@ async def on_ready():
     for extension in extensions:
         bot.load_extension(extension)
 
-@bot.event
-async def on_member_join(member):
-    if member.guild == bot.get_guild(config.GUILD_ID):
-        await member.create_dm()
-        await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-1.png'))
-        await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-2.png'))
-        await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-3.png'))
-        await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-4.png'))
-        await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-5.png'))
+# @bot.event
+# async def on_member_join(member):
+#     if member.guild == bot.get_guild(config.GUILD_ID):
+#         await member.create_dm()
+#         await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-1.png'))
+#         await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-2.png'))
+#         await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-3.png'))
+#         await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-4.png'))
+#         await member.dm_channel.send(file=discord.File('pikachu/assets/images/op-5.png'))
 
 if __name__ == "__main__":
     bot.run(TOKEN, bot=True, reconnect=True)
